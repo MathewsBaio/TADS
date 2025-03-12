841d5298ae69c2c6bdbf9ce51d20ae49caef4b24
Mathews <baiomathews@gmail.com>
2025-03-12 19:26:20 -0300

    matriz

--- gameoflife/jogovida.py
+++ gameoflife/jogovida.py
@@ -3,3 +3,21 @@ import matplotlib.pyplot as fig
 from matplotlib import animation, cm 
 import random 
 
+fig.close('all')
+
+r = 80 #linhas
+c = 80 #colunas
+
+matriz = np.zeros((r+1, c+1))
+novo_matriz = np.zeros((r+1, c+1))
+
+
+matriz[4,1] = 1
+matriz[4,2] = 1
+matriz[6,2] = 1
+matriz[5,3] = 1
+matriz[5,4] = 1
+matriz[4,5] = 1
+matriz[6,5] = 1
+matriz[6,1] = 1
+matriz[5,6] = 1
